b1e6f2e13a973fa5e0a6cddc089646d70fc6d2f2
marcepanowyy <101901301+marcepanowyy@users.noreply.github.com>
2022-09-07 23:34:11 +0200

    Create ex1.sql

---
+++ LaboratoryExercises/Lab1/ex1.sql
@@ -0,0 +1,9 @@
+SELECT [CompanyName], [Address] FROM [Customers]
+
+SELECT [LastName], [HomePhone] FROM [Employees]
+
+SELECT [ProductName], [UnitPrice] from [Products]
+
+SELECT [CategoryName], [Description] FROM [Categories]
+
+SELECT [CompanyName], [Address] FROM [Suppliers]

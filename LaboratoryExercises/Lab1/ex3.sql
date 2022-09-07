e5bbc2d5899858997af4aeb8a44424112bb708f5
marcepanowyy <101901301+marcepanowyy@users.noreply.github.com>
2022-09-07 23:35:11 +0200

    Create ex3.sql

---
+++ LaboratoryExercises/Lab1/ex3.sql
@@ -0,0 +1,9 @@
+SELECT [ProductName] FROM [Products] WHERE [QuantityPerUnit] LIKE '%bottle%'
+
+SELECT [LastName], [Title] FROM [Employees] WHERE [LastName] LIKE '[B-L]%'
+
+SELECT [LastName], [Title] FROM [Employees] WHERE [LastName] LIKE '[BL]%'
+
+SELECT [CategoryName] FROM [Categories] WHERE [Description] LIKE  '%,%'
+
+SELECT [CompanyName] FROM [Customers] WHERE [CompanyName] LIKE '%Store%'
